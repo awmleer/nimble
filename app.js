c5c5fbded6f60617302f7abce7eb8adaace5e758
HaoGuangBo <haoguangbo@yeah.net>
2017-01-29 09:42:21 +0800

    v-for

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ let vm = new Vue({
   el: '#app',
   data:{
     keyword:'',
+    items:[
+      {keyword:'test',path:'/Users/Hao'},
+      {keyword:'hahah',path:'/Users/Hao'},
+      {keyword:'work',path:'/Users/Hao'}
+    ],
     a:1
   },
   methods:{
     doSomething: function () {
       $('#keyword')[0].focus();
+      this.a=22;
     }
   }
 });
